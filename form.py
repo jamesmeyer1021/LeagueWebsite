870c7396a1823afc8d620c61072e08856f3c1601
James Meyer <56805328+jamesmeyer1021@users.noreply.github.com>
2023-12-29 18:38:28 -0500

    Adding new page for once account is added.

--- form.py
+++ form.py
@@ -1,9 +1,13 @@
 # importing Flask and other modules
-from flask import Flask, request, render_template 
+from flask import Flask, request, render_template, redirect, url_for
 import functions
+import os
+
+user = os.environ.get('USERNAME')
 
 # Flask constructor
-app = Flask('__name__', template_folder = r'C:\Users\James\Desktop\Project') 
+app = Flask('__name__', template_folder = rf'C:\Users\{user}\Desktop\Project') 
+summonerName = ""
 
 # A decorator used to tell the application
 # which URL is associated function
@@ -12,8 +16,16 @@ def getSummonerName():
    if request.method == "POST":
       # getting input with name = fname in HTML form
       summonerName = request.form.get("sname")
-      return functions.levelCheck(summonerName)
+      return redirect(url_for('tellingSummonerStats', summonerName=summonerName))
    return render_template("index.html")
+
+# A decorator used to tell the application
+# which URL is associated function
+@app.route('/stats', methods =["GET", "POST"])
+def tellingSummonerStats():
+   summonerName = request.args.get('summonerName')
+   levelCheck = functions.levelCheck(summonerName)
+   return render_template("summonerStats.html", levelCheck = levelCheck)
 
 if __name__=='__main__':
    app.debug = True
--- functions.py
+++ functions.py
@@ -3,8 +3,9 @@ import secret
 
 #function to check the level of the Summoner
 def levelCheck(summonerName):
+    print(summonerName)
     r = requests.get(f'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={secret.APIKEY}')
-
+    print(r.text)
     if r.status_code != 200:
         return "That wasn't it"        
     
@@ -12,6 +13,6 @@ def levelCheck(summonerName):
     level = formattedData.get("summonerLevel")
 
     if level > 100:
-        return "You're a low level"
-    else:
         return "You're not a low level"
+    else:
+        return "You're a low level"
